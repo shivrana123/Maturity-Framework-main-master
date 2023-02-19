5371cbc4e26beb8f2d79a40b0ad6524bbfda0aac
Shivani Rana <shivrana521@gmail.com>
2023-02-19 21:53:39 +0530

    b

--- src/components/layouts/IntroductionPage.js
+++ src/components/layouts/IntroductionPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './Header';
+import Header from './Header'; 
 import styles from '../../styles/layouts/IntroductionPage.module.css';
 import NavigationButton from '../common/NavigationButton';
 import ContentInputBox from '../common/ContentInputBox';
